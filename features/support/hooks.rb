d68bafec76eaa3edc25e238bf486acdf1086b741
SebastianLazarte <72448046+SebastianLazarte@users.noreply.github.com>
2022-06-03 22:04:25 -0400

    Fix

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,6 +8,122 @@ Before '@maximize' do
   page.driver.browser.manage.window.maximize
 end
 
-Before '@ViewEvent' do
-  page.driver.browser.manage.window.maximize
+Before '@TestingAvailableV' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERV']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "No disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
+end
+
+Before '@TestingNotAvailableV' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERV']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "Estoy disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
+end
+
+Before '@TestingAvailableL' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERL']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "No disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
+end
+
+Before '@TestingAvailableL' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERV']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "Estoy disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
+end
+
+Before '@TestingAvailableC' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERV']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "No disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
+end
+
+Before '@TestingAvailableC' do
+  visit('https://testing-start.web.app/login')
+  fill_in 'email', :with => ENV['USERV']
+  fill_in 'password', :with => ENV['PSW']
+  xpath_Ini = '/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/button'
+  find(:xpath, xpath_Ini).click
+  sleep 3
+  visit('https://testing-start.web.app/cuenta')
+  sleep 3
+  texto = find(:xpath, '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label/span[2]').text
+  if (texto == "Estoy disponible")
+    xpath = '/html/body/div/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/label'
+    find(:xpath, xpath).click
+  end
+  xpath_Exit = '/html/body/div/div[2]/header/div[1]/div[3]/div/div'
+  find(:xpath, xpath_Exit).click
+  xpath_logout = '/html/body/div[2]/div/button'
+  find(:xpath, xpath_logout).click
 end
